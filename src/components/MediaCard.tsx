90a61c59913b72562f598d5333f867f03234eb78
Shirish <shirishsigdyal@lftechnology.com>
2025-02-19 12:34:06 +0545

    feat: Create gradient and backdrop image view.

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -3,64 +3,69 @@ import React from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import SearchIcon from "@mui/icons-material/Search";
 
-import {
-  Button,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  Box,
-} from "@mui/material";
+import { Button, Typography, Box, Paper } from "@mui/material";
 import { MediaItemProps } from "../models/model";
 
 const MediaCard: React.FC<MediaItemProps> = ({ item, name, date }) => {
   return (
-    <Card
-      key={item?.id}
+    <Paper
       sx={{
-        height: "500px",
-        backgroundColor: "#121212",
-        color: "white",
+        backgroundImage: `url(https://image.tmdb.org/t/p/w780${item?.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        opacity: 0.7,
+        position: "relative",
+        zIndex: 1,
         borderRadius: 2,
-        overflow: "hidden",
         mb: 4,
+        height: 400,
       }}>
-      <CardMedia
-        component="img"
-        height="250"
-        image={`https://image.tmdb.org/t/p/w780${item?.backdrop_path}`}
-        alt="The Gorge Movie Poster"
-        sx={{ opacity: 0.7 }}
-      />
-      <CardContent sx={{ position: "relative", padding: 2 }}>
-        <Typography variant="h5" fontWeight="bold">
-          {name}
-        </Typography>
-        <Typography variant="h6" color="goldenrod">
-          {item.vote_average} / 10
-        </Typography>
-        <Typography variant="subtitle2" color="gray" gutterBottom>
-          {date}
-        </Typography>
-        <Typography variant="body2" color="white" paragraph>
-          {item.overview}
-        </Typography>
-        <Box display="flex" gap={2} mt={2}>
-          <Button
-            variant="contained"
-            color="error"
-            startIcon={<PlayArrowIcon />}>
-            Watch Trailer
-          </Button>
-          <Button
-            variant="contained"
-            color="success"
-            startIcon={<SearchIcon />}>
-            Search Torrent
-          </Button>
+      <Box
+        sx={{
+          position: "absolute",
+          padding: 2,
+          zIndex: 2,
+          width: "60%",
+          height: "100%",
+          backgroundBlendMode: "multiply",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          background:
+            "linear-gradient(to right, #0d0d0c 50%, transparent 100%)",
+        }}>
+        <Box>
+          <Typography variant="h5" fontWeight="bold" sx={{ color: "white" }}>
+            {name}
+          </Typography>
+          <Typography variant="h6" color="goldenrod">
+            {item.vote_average} / 10
+          </Typography>
+          <Typography variant="subtitle2" color="gray" gutterBottom>
+            {date}
+          </Typography>
         </Box>
-      </CardContent>
-    </Card>
+        <Box>
+          <Typography variant="body2" color="white" paragraph>
+            {item.overview}
+          </Typography>
+          <Box display="flex" gap={2} mt={2}>
+            <Button
+              variant="contained"
+              color="error"
+              startIcon={<PlayArrowIcon />}>
+              Watch Trailer
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<SearchIcon />}>
+              Search Torrent
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Paper>
   );
 };
 
